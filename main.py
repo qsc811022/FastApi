c88e7ee7ea0071bb766eac507930ee890a969644
TEDLIUCOMPUTER\asus <qsc811022@gmail.com>
2022-09-25 21:58:38 +0800

    Example_CRUD

--- main.py
+++ main.py
@@ -1,9 +1,25 @@
 #main.py
 from fastapi import FastAPI
+import schemas;
+
 app = FastAPI()
+fakeDatabase = {
+    1:{'task':'Clean car'},
+    2:{'task':'Write blog'},
+    3:{'task':'Start stream'},
+}
+
 @app.get("/")
-
-
-
 def getItems():
-    return ['Item 1', 'Item 2', 'Item 3']
+    return fakeDatabase
+
+@app.get("/{id}")
+def getItems(id:int):
+    return fakeDatabase[id]
+
+
+@app.post("/")
+def addItems(task:schemas.Item):
+    newId = len(fakeDatabase.keys())+1
+    fakeDatabase[newId] = {"task":task}
+    return fakeDatabase
